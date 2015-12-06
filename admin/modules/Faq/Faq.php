0c3416ab27f8ea21e538e475cc836faa04cbc1a1
PHP-Mods <phpmods@users.noreply.github.com>
2015-12-06 22:14:49 +0200

    Update Faq.php

--- admin/modules/Faq/Faq.php
+++ admin/modules/Faq/Faq.php
@@ -1,9 +1,9 @@
 <?php
 ///////////////////////////////////////////////
-///      FAQSystem v1.1 by php-mods.eu      ///
+///      FAQSystem v1.2 by php-mods.eu      ///
 ///            Author php-mods.eu           ///
-///           Packed at 19/6/2014           ///
-///     Copyright (c) 2014, php-mods.eu     ///
+///           Packed at 6/12/2015           ///
+///     Copyright (c) 2015, php-mods.eu     ///
 ///////////////////////////////////////////////
 
 class FAQ extends CodonModule {
@@ -16,84 +16,85 @@ class FAQ extends CodonModule {
     }
 	public function index() {
 		$this->set('categories', FAQData::getallcat());
-		$this->render('faq/faq_index.tpl');
+		$this->show('faq/faq_index');
 	}
 	public function administrate($id) {
 		$this->set('info', FAQData::getcatinfo($id));
 		$this->set('faqina', FAQData::faqincat($id));
-		$this->render('faq/faq_administrate.tpl');
+		$this->show('faq/faq_administrate');
 	}
 	public function faqcatadd() {
-	    $this->render('faq/faq_category_add.tpl');
+	    $this->show('faq/faq_category_add');
     }
-    public function faqcatadd_a() {
+    public function faq_insert() {
 		if($this->post->title == '') {
 			$this->set('message', 'Some fields are missing! Fill them all please.');
-			$this->render('core_error.tpl');
+			$this->show('core_error');
 			return;
 		}	
-	    $val = FAQData::faqcatadd($this->post->title, $this->post->active);
-	 	$this->set('message', 'FAQ Category Added Successfully!');
-		$this->render('core_success.tpl');
-		$this->set('categories', FAQData::getallcat());
-		$this->render('faq/faq_index.tpl');
-		LogData::addLog(Auth::$userinfo->pilotid, "Added a faq category named \"{$this->post->title}\"");
+		$title = DB::escape($this->post->title);
+		$active = DB::escape($this->post->active);
+	    FAQData::faqcatadd($title, $active);
+	 	$this->set('message', 'FAQ Category Added');
+		$this->show('core_success');
+		self::index();
+		LogData::addLog(Auth::$userinfo->pilotid, "Added a faq category titled \"{$title}\"");
     }
 	public function faqcaedit($id) {
 		$this->set('faqinfo', FAQData::getcatinfo($id));
-		$this->render('faq/faq_category_edit.tpl');
+		$this->show('faq/faq_category_edit');
 	}
 	public function faqcaedit_db() {
 		if($this->post->title == '') {
 			$this->set('message', 'Some fields are missing! Fill them all please.');
-			$this->render('core_error.tpl');
+			$this->show('core_error');
 			return;
 		}
 		$title = DB::escape($this->post->title);
 		$active = DB::escape($this->post->active);
 		$id = DB::escape($this->post->id);
 		FAQData::edit_category($title, $active, $id);
-		$this->set('message', 'FAQ Category Updated!');
-		$this->render('core_success.tpl');
-		$this->set('info', FAQData::getcatinfo($id));
-		$this->set('faqina', FAQData::faqincat($id));
-		$this->render('faq/faq_administrate.tpl');
-		LogData::addLog(Auth::$userinfo->pilotid, "Updated the $title FAQ category.");
+		$this->set('message', 'FAQ Category Updated');
+		$this->show('core_success');
+		self::administrate($id);
+		LogData::addLog(Auth::$userinfo->pilotid, "Updated the \"$title\" FAQ category.");
 	}
 	public function faqcategorydelete($id) {
+		FAQData::deletefaq_by_cat($id);
 	    FAQData::deletefaqcategory($id);
-	    $this->set('message', 'Faq Category Deleted!');
-	    $this->render('core_success.tpl');
-	    $this->set('categories', FAQData::getallcat());
-	    $this->render('faq/faq_index.tpl');
+	    $this->set('message', 'Faq Category Deleted');
+	    $this->show('core_success');
+	    self::index();
 	    LogData::addLog(Auth::$userinfo->pilotid, "Deleted a FAQ Category.");
 	}
 	public function faqadd($id) {
 		$this->set('info', FAQData::getcatinfo($id));
-	    $this->render('faq/faq_add.tpl');
+	    $this->show('faq/faq_add');
     }
     public function faqadd_a(){
 		if($this->post->title == '' or $this->post->text == '') {
 			$this->set('message', 'Some fields are missing! Fill them all please.');
-			$this->render('core_error.tpl');
+			$this->show('core_error');
 			return;
 		}	
-		$val = FAQData::faqadd($this->post->title, $this->post->text, $this->post->category, $this->post->active);
-	 	$this->set('message', 'FAQ Added Successfully!');
-		$this->render('core_success.tpl');
-		$this->set('info', FAQData::getcatinfo($this->post->category));
-		$this->set('faqina', FAQData::faqincat($this->post->category));
-		$this->render('faq/faq_administrate.tpl');
-		LogData::addLog(Auth::$userinfo->pilotid, "Added a faq with title \"{$this->post->title}\"");
+		$title = DB::escape($this->post->title);
+		$text = DB::escape($this->post->text);
+		$category = DB::escape($this->post->category);
+		$active = DB::escape($this->post->active);
+		FAQData::faqadd($title, $text, $category, $active);
+	 	$this->set('message', 'FAQ Added');
+		$this->show('core_success');
+		self::administrate($category);
+		LogData::addLog(Auth::$userinfo->pilotid, "Added a faq titled \"{$title}\"");
 	}
 	public function faqedit($id) {
 		$this->set('faqinfo', FAQData::faqeditform($id));
-		$this->render('faq/faq_edit.tpl');
+		$this->show('faq/faq_edit');
 	}
 	public function faqedit_db() {
 		if($this->post->title == '' or $this->post->text == '') {
 			$this->set('message', 'Some fields are missing! Fill them all please.');
-			$this->render('core_error.tpl');
+			$this->show('core_error');
 			return;
 		}
 		$category = DB::escape($this->post->category);
@@ -102,20 +103,16 @@ class FAQ extends CodonModule {
 		$text = DB::escape($this->post->text);
 		$id = DB::escape($this->post->id);
 		FAQData::edit_faq($title, $active, $text, $id);
-		$this->set('message', 'FAQ Updated!');
-		$this->render('core_success.tpl');
-		$this->set('info', FAQData::getcatinfo($category));
-		$this->set('faqina', FAQData::faqincat($category));
-		$this->render('faq/faq_administrate.tpl');
-		LogData::addLog(Auth::$userinfo->pilotid, "Updated the $title FAQ.");
+		$this->set('message', 'FAQ Updated');
+		$this->show('core_success');
+		self::administrate($category);
+		LogData::addLog(Auth::$userinfo->pilotid, "Updated the \"$title\" FAQ.");
 	}
 	public function faqdelete($id, $cat) {
-	  FAQData::deletefaq($id);
-	  $this->set('message', 'FAQ Deleted!');
-	  $this->render('core_success.tpl');
-	  $this->set('info', FAQData::getcatinfo($cat));
-		$this->set('faqina', FAQData::faqincat($cat));
-		$this->render('faq/faq_administrate.tpl');
-	  LogData::addLog(Auth::$userinfo->pilotid, "Deleted a FAQ.");
+		FAQData::deletefaq($id);
+		$this->set('message', 'FAQ Deleted');
+		$this->show('core_success');
+		self::administrate($cat);
+		LogData::addLog(Auth::$userinfo->pilotid, "Deleted a FAQ.");
 	}
 }
